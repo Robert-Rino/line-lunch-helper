129d654f4be5321a1b032f0d08661ae563686589
Robert-Rino <a901002666@gmail.com>
2017-02-01 01:05:41 +0800

    test

--- app.rb
+++ app.rb
@@ -49,7 +49,7 @@ post '/callback' do
         }
 
         @menu.each_with_index do |dish, index|
-          message[:text] =+ "#{index}. #{dish} \n"
+          message[:text] =+ "#{index} #{dish} \n"
         end
         # message = {
         #   type: 'template',
