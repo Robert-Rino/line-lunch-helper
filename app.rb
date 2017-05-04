3a2d3dc06239db4d61e8bcdc9aa81bae79624e9c
Robert-Rino <a901002666@gmail.com>
2017-05-04 20:17:51 +0800

    test

--- app.rb
+++ app.rb
@@ -95,7 +95,7 @@ post '/callback' do
           puts response
           reply_message = {
             "type": "text",
-            "text": response[:data]
+            "text": response["data"]
           }
           # reply_message ={
           #   "type": "text",
